ee57b53a242148859139e6c23701e803254ef874
Shawn Tien <hightian@gmail.com>
2016-12-26 01:39:24 +0800

    update for api

---
+++ app.py
@@ -0,0 +1,22 @@
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/search/<string:name>', methods=['POST'])
+def index(name):
+    origin = 'http://music.163.com'
+    url = origin + '/api/search/suggest/web'
+    url += '?s=%s&limit=30&offset=0&type=1' % name    # set search params
+
+    headers = {'Origin': origin, 'Referer': origin, 'Accept': 'application/json', 'Content-Type': 'application/json'}
+
+    req = requests.post(url, headers=headers)
+
+    print '>', url
+    return req.text
+
+
+if __name__ == "__main__":
+    app.run()
