06c50358270564dcf38753ac1f47816c5ec73350
LeonaYoung <leona.y.young@gmail.com>
2017-03-13 00:01:46 +0800

    update readme

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ API 部分参考了 [NeteaseCloudMusicApi](https://github.com/Binaryify/NeteaseC
 ### 启动服务
 # run server
 python app.py
+抓取网易云音乐数据
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+#...
 @app.route('/search/<string:name>', methods=['POST'])
 def search(name):
     location = '/api/search/suggest/web'
